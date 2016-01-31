67c47541a9f26f963bf2737ace90cab42047d80e
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-01-30 23:55:38 -0700

    Initial commit

---
+++ Testwise.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired #, InputRequired ,Required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'N0tHingIsImpo5Sibl3'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+# Class name ( Type of Object )
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',current_time=datetime.utcnow(),form=form, name=name)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run()
